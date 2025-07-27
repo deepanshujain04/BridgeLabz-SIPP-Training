cda13f99210041e869848e349c563da601160e91
deepanshujain04 <jaindeepanshu2004@gmail.com>
2025-07-27 22:58:26 +0530

    Delete generics files from Generics_And_Collection branch

--- Generics/MealPlanGenerator/src/mealplan/MealPlanGenerator.java
+++
@@ -1,9 +0,0 @@
-package mealplan;
-
-public class MealPlanGenerator {
-    public static <T extends MealPlan> Meal<T> generateMealPlan(T mealType) {
-        // You can add validation here (e.g., nutrition rules, time constraints, etc.)
-        System.out.println("Generating personalized meal plan...");
-        return new Meal<>(mealType);
-    }
-}
